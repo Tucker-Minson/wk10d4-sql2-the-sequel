dc452d6fc1baeef3926aa29c784237489f9b08c2
Tucker.Minson <Rtuckm@gmail.com>
2023-02-02 15:08:00 -0800

    phase 3 fix

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -80,3 +80,19 @@ VALUES
   (7, 'Cheetos'),
   (8, 'Crinkle Ball'),
   (8, 'Yarn');
+
+
+-- Find Hermione's cats
+SELECT cats.name
+ FROM cats
+ JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+ JOIN owners ON (cat_owners.owner_id = owners.id)
+WHERE owners.first_name = 'Hermione';
+
+-- Find All the Toys for Hermione's cats
+SELECT toys.name
+ FROM toys
+ JOIN cats ON (cats.id = toys.cat_id)
+ JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+ JOIN owners ON (cat_owners.owner_id = owners.id)
+WHERE owners.first_name = 'Hermione';
